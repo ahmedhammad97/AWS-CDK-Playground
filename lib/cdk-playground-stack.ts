bc90876804ee3524475123fa4778e683f1c0664e
Ahmed Hammad <hammad97official@gmail.com>
2020-07-18 01:03:27 +0200

    Add basic infrastructure (vpc, esc cluster, and fargate service)

--- lib/cdk-playground-stack.ts
+++ lib/cdk-playground-stack.ts
@@ -1,9 +1,22 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
 
 export class CdkPlaygroundStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'CDKPlaygroundVPC');
+    const cluster = new ecs.Cluster(this, 'CDKPlaygroundCluster', {vpc: vpc});
+    const service = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 
+    'CDKPlaygroundService', {
+      cluster: cluster,
+      listenerPort: 4545,
+      taskImageOptions: { 
+        image: ecs.ContainerImage.fromAsset(__dirname + '/../main_service')
+      }
+    });
+
   }
 }
